0d043aa94336bf3a40fc0cba98e61e3948e5fb31
Laura Castillo <lauracasdu@gmail.com>
2022-07-14 18:22:38 +0200

    data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,8 +4,7 @@ INTO
   users
   (user_Name, user_Img)
 VALUES
- ('Adventurer', 'https://img.freepik.com/free-vector/portrait-girl-safari-outfit-cartoon-character-sticker_1308-67917.jpg');
-
+ ('Adventurer', 'https://thumbs.dreamstime.com/b/cute-cartoon-character-girl-avatar-white-background-flat-vector-illustration-eps-cute-cartoon-character-girl-avatar-white-181634274.jpg');
 
 
 INSERT
@@ -13,5 +12,66 @@ INTO
   moments
   (img_Url, title, description, creator_id)
 VALUES
-  ('https://cdn1.intriper.com/wp-content/uploads/2020/04/21144613/black-forest-alemania-germany.jpg', 'Island', 'Lorem ipsum...', 1);
+  ('https://cdn1.intriper.com/wp-content/uploads/2020/04/21144613/black-forest-alemania-germany.jpg', 'Spain', 'Garrotxa', 1);
 
+
+INSERT
+INTO
+  moments
+  (img_Url, title, description, creator_id)
+VALUES
+  ('https://www.civitatis.com/blog/wp-content/uploads/2021/07/jungla-costa-rica.jpg', 'Costa Rica', 'Wild nature', 1);
+
+
+INSERT
+INTO
+  moments
+  (img_Url, title, description, creator_id)
+VALUES
+  ('https://i.pinimg.com/originals/93/21/b2/9321b2e3f66b12777ff7e98a9860a597.jpg', 'Guitar', 'Nature', 1);
+
+
+INSERT
+INTO
+  moments
+  (img_Url, title, description, creator_id)
+VALUES
+  ('https://media.timeout.com/images/105856246/750/422/image.jpg', 'Slovenia', 'Predjama Castle', 1);
+
+
+INSERT
+INTO
+  moments
+  (img_Url, title, description, creator_id)
+VALUES
+  ('https://www.lavanguardia.com/r/GODO/LV/p6/WebSite/2019/08/16/Recortada/img_hmacia_20190816-121538_imagenes_lv_getty_istock-952926670-kD4C-U464100269612nAB-992x558@LaVanguardia-Web.jpg', 'Ice-cream', 'Italy', 1);
+
+
+INSERT
+INTO
+  moments
+  (img_Url, title, description, creator_id)
+VALUES
+  ('https://thumb.mp-farm.com/75973771/preview.jpg', 'Norway', 'Landscape', 1);
+
+
+INSERT
+INTO
+  moments
+  (img_Url, title, description, creator_id)
+VALUES
+  ('https://sunridgefarms.com/wp-content/uploads/2018/03/woman-doing-yoga-on-mountain-small.jpg', 'Yoga', 'Yoga on mountain', 1);
+
+INSERT
+INTO
+  moments
+  (img_Url, title, description, creator_id)
+VALUES
+  ('https://motor.elpais.com/wp-content/uploads/2018/06/mercedes-vito-marco-polo.jpg', 'Van', 'Van adventure', 1);
+
+INSERT
+INTO
+  comments
+  (comment, moment_id, creator_id)
+VALUES
+  ('Very Beautiful', 1, 1);
