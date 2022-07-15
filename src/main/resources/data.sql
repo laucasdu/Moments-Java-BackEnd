4521aef6f7dc31b87ef08b97fd84370f00b50269
Laura Castillo <lauracasdu@gmail.com>
2022-07-15 02:56:06 +0200

    data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,7 +4,7 @@ INTO
   users
   (user_Name, user_Img)
 VALUES
- ('Adventurer', 'https://thumbs.dreamstime.com/b/cute-cartoon-character-girl-avatar-white-background-flat-vector-illustration-eps-cute-cartoon-character-girl-avatar-white-181634274.jpg');
+ ('ADVENTURER', 'https://thumbs.dreamstime.com/b/cute-cartoon-character-girl-avatar-white-background-flat-vector-illustration-eps-cute-cartoon-character-girl-avatar-white-181634274.jpg');
 
 
 INSERT
@@ -12,16 +12,14 @@ INTO
   moments
   (img_Url, title, description, creator_id)
 VALUES
-  ('https://cdn1.intriper.com/wp-content/uploads/2020/04/21144613/black-forest-alemania-germany.jpg', 'Spain', 'Garrotxa', 1);
-
+  ('https://viajes.nationalgeographic.com.es/medio/2019/07/19/acantilados-de-moher-ser-yo-tiene-sus-privilegios_1b1cd0a5_1500x994.jpg', 'Spain', 'Asturias', 1);
 
 INSERT
 INTO
   moments
   (img_Url, title, description, creator_id)
 VALUES
-  ('https://www.civitatis.com/blog/wp-content/uploads/2021/07/jungla-costa-rica.jpg', 'Costa Rica', 'Wild nature', 1);
-
+  ('https://photos.altai-travel.com/1920x1040/photo-dun-pont-suspendu-au-pied-du-volcan-arenal-au-costa-rica-8257.jpg', 'Costa Rica', 'Wild nature', 1);
 
 INSERT
 INTO
@@ -30,7 +28,6 @@ INTO
 VALUES
   ('https://i.pinimg.com/originals/93/21/b2/9321b2e3f66b12777ff7e98a9860a597.jpg', 'Guitar', 'Nature', 1);
 
-
 INSERT
 INTO
   moments
@@ -38,22 +35,12 @@ INTO
 VALUES
   ('https://media.timeout.com/images/105856246/750/422/image.jpg', 'Slovenia', 'Predjama Castle', 1);
 
-
 INSERT
 INTO
   moments
   (img_Url, title, description, creator_id)
 VALUES
-  ('https://www.lavanguardia.com/r/GODO/LV/p6/WebSite/2019/08/16/Recortada/img_hmacia_20190816-121538_imagenes_lv_getty_istock-952926670-kD4C-U464100269612nAB-992x558@LaVanguardia-Web.jpg', 'Ice-cream', 'Italy', 1);
-
-
-INSERT
-INTO
-  moments
-  (img_Url, title, description, creator_id)
-VALUES
-  ('https://thumb.mp-farm.com/75973771/preview.jpg', 'Norway', 'Landscape', 1);
-
+  ('https://www.guiadealemania.com/wp-content/uploads/2009/08/selva-bach.jpg', 'Germany', 'Black Forest', 1);
 
 INSERT
 INTO
@@ -61,6 +48,13 @@ INTO
   (img_Url, title, description, creator_id)
 VALUES
   ('https://sunridgefarms.com/wp-content/uploads/2018/03/woman-doing-yoga-on-mountain-small.jpg', 'Yoga', 'Yoga on mountain', 1);
+
+INSERT
+INTO
+  moments
+  (img_Url, title, description, creator_id)
+VALUES
+  ('https://thumb.mp-farm.com/75973771/preview.jpg', 'Norway', 'Landscape', 1);
 
 INSERT
 INTO
@@ -75,3 +69,15 @@ INTO
   (comment, moment_id, creator_id)
 VALUES
   ('Very Beautiful', 1, 1);
+INSERT
+INTO
+  comments
+  (comment, moment_id, creator_id)
+VALUES
+  ('Amazing place', 1, 1);
+INSERT
+INTO
+  comments
+  (comment, moment_id, creator_id)
+VALUES
+  ('Music oh yeah!', 3, 1);
