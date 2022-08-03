aa833c5fae4c8a6309d7adc0a6d4297c1cfd3cc7
Laura Castillo <lauracasdu@gmail.com>
2022-08-03 14:57:22 +0200

    seed Data Service

---
+++ src/main/java/com/factoria/moments/faker/SeedDataService.java
@@ -0,0 +1,40 @@
+package com.factoria.moments.faker;
+
+import com.factoria.moments.models.Moment;
+import com.factoria.moments.repositories.IMomentRepository;
+import com.factoria.moments.repositories.IUserRepository;
+import com.factoria.moments.services.IUserService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+import javax.annotation.PostConstruct;
+
+@Component
+public class SeedDataService { //quan s'engega l'aplicació s'injecta el seed
+
+    private IMomentRepository momentRepository;
+    private IUserRepository userRepository;
+    private PasswordEncoder encoder;
+
+
+    public SeedDataService(IMomentRepository momentRepository, IUserRepository userRepository, PasswordEncoder encoder) {
+        this.momentRepository = momentRepository;
+        this.userRepository = userRepository;
+        this.encoder = encoder;
+    }
+
+
+    @PostConstruct // quan construeix aquest bean (o component) Seed, s'executarà la funció createDate, just quan s'executarà l'app
+    public void createData(){
+        if(!momentRepository.findAll().isEmpty()) return;
+
+        var user = userRepository.findById(1L).get();
+        var moment = new Moment();
+        moment.setCreator(user);
+        moment.setTitle("testMoment");
+        moment.setDescription("testDescription");
+        moment.setImgUrl("img.jpg");
+        momentRepository.save(moment);
+
+    }
+
+}
